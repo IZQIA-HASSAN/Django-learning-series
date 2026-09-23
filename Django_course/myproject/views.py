6ceb79c1ecb333dd691f5e1266de88abf2101849
ls <izqiaulhassan166@gmail.com>
2026-09-23 20:43:25 +0500

    Personal Navigator

--- Django_course/myproject/views.py
+++ Django_course/myproject/views.py
@@ -2,23 +2,11 @@
 from django.http import HttpResponse
 import os
 
-# def index(request):
-#     return HttpResponse("hello")
-
-
-# def about(request):
-#     return HttpResponse("this is about section")
-
-
 def index(request):
-    file_path = os.path.join(os.path.dirname(__file__) , 'one.txt')
-    try:
-        with open(file_path , 'r' , encoding='utf-8') as f :
-            content = f.read()
-    except FileNotFoundError:
-        content = "file not found error"
+    return HttpResponse('''<h1>Izqia is a Django Developer</h1>  <a href="https://github.com/IZQIA-HASSAN/Django-learning-series">Visit github repo for updated code.</a>''')
 
-    return HttpResponse(f"<pre>{content}</pre>")
 
 # def about(request):
 #     return HttpResponse("this is about section")
+
+
